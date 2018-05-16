ed615458b5a922bf378acb47dde52e7d620caea5
John Strack <Braveheart22@GMail.com>
2018-05-16 17:25:36 -0400

    Add expense data to database.

--- app/src/main/java/com/johnstrack/mybank/AddExpenseActivity.kt
+++ app/src/main/java/com/johnstrack/mybank/AddExpenseActivity.kt
@@ -2,7 +2,12 @@ package com.johnstrack.mybank
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.johnstrack.mybank.Utilities.EXPENSES_REF
+import kotlinx.android.synthetic.main.activity_add_expense.*
 
 class AddExpenseActivity : AppCompatActivity() {
 
@@ -13,6 +18,19 @@ class AddExpenseActivity : AppCompatActivity() {
 
     fun addExpenseClicked (view: View) {
 
+        val data = HashMap<String, Any>()
+        data["itemName"] = addCategoryText.text.toString()
+        data["price"] = addPriceText.text.toString()
+        data["category"] = addCategoryText.text.toString()
+        data["timestamp"] = FieldValue.serverTimestamp()
+        data["username"] = "Some User"
+        FirebaseFirestore.getInstance().collection(EXPENSES_REF).add(data)
+                .addOnSuccessListener {
+                    finish()
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Error", "Could not add data: ${e.localizedMessage}")
+                }
     }
 
     fun cancelBtnClicked (view: View) {
---
+++ app/src/main/java/com/johnstrack/mybank/Utilities/Constants.kt
@@ -0,0 +1,6 @@
+package com.johnstrack.mybank.Utilities
+
+/**
+ * Created by John on 5/16/2018 at 5:15 PM.
+ */
+const val EXPENSES_REF = "expenses"
