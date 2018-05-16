fe83789a045a91864c5a130857b842e21c469e19
John Strack <Braveheart22@GMail.com>
2018-05-16 15:10:43 -0400

    Add expense layout created.

--- app/src/main/java/com/johnstrack/mybank/AddExpenseActivity.kt
+++ app/src/main/java/com/johnstrack/mybank/AddExpenseActivity.kt
@@ -2,11 +2,20 @@ package com.johnstrack.mybank
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class AddExpenseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_expense)
+    }
+
+    fun addExpenseClicked (view: View) {
+
+    }
+
+    fun cancelBtnClicked (view: View) {
+        finish()
     }
 }
