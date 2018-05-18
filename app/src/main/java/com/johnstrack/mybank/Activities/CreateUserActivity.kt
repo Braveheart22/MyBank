db89e51a20b5b3c05690ba6c1a4f0a316fc16e1e
John Strack <Braveheart22@GMail.com>
2018-05-18 15:20:38 -0400

    Login user activity created.

---
+++ app/src/main/java/com/johnstrack/mybank/Activities/CreateUserActivity.kt
@@ -0,0 +1,13 @@
+package com.johnstrack.mybank.Activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.johnstrack.mybank.R
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+}
---
+++ app/src/main/java/com/johnstrack/mybank/Activities/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.johnstrack.mybank.Activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.johnstrack.mybank.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginButtonClicked (view: View) {
+
+    }
+
+    fun createUserButtonClicked (view: View) {
+
+    }
+}
