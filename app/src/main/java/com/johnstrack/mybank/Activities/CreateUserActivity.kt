a0a3c7e453fc7363ad9ac83a8a42bf87191cc3ab
John Strack <Braveheart22@GMail.com>
2018-05-18 15:28:49 -0400

    Create user activity created.

--- app/src/main/java/com/johnstrack/mybank/Activities/CreateUserActivity.kt
+++ app/src/main/java/com/johnstrack/mybank/Activities/CreateUserActivity.kt
@@ -2,6 +2,7 @@ package com.johnstrack.mybank.Activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.johnstrack.mybank.R
 
 class CreateUserActivity : AppCompatActivity() {
@@ -9,5 +10,13 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+    }
+
+    fun createUserButtonClicked(view: View) {
+
+    }
+
+    fun cancelButtonClicked (view: View) {
+        finish()
     }
 }
