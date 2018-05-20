a05d3edad44db2af71767e47995b11a9954b03cc
John <Braveheart22@GMail.com>
2018-05-20 12:29:38 -0400

    Implement login activity.

--- app/src/main/java/com/johnstrack/mybank/Activities/LoginActivity.kt
+++ app/src/main/java/com/johnstrack/mybank/Activities/LoginActivity.kt
@@ -3,18 +3,33 @@ package com.johnstrack.mybank.Activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import com.johnstrack.mybank.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
     }
 
     fun loginButtonClicked (view: View) {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
 
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    finish()
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Error", "Could not login user: ${e.localizedMessage}")
+                }
     }
 
     fun createUserButtonClicked (view: View) {
