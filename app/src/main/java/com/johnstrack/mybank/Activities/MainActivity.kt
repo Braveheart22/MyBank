78431ed26acd38798609aa1e9b2c9e7487686aaf
John Strack <Braveheart22@GMail.com>
2018-05-30 17:42:37 -0400

    Implement delete of all expenses (from menu).

--- app/src/main/java/com/johnstrack/mybank/Activities/MainActivity.kt
+++ app/src/main/java/com/johnstrack/mybank/Activities/MainActivity.kt
@@ -22,6 +22,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.lang.String.format
 import java.util.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity(), ExpenseDeleteItemClickListener {
 
@@ -30,7 +31,7 @@ class MainActivity : AppCompatActivity(), ExpenseDeleteItemClickListener {
     private lateinit var expenseAdapter: ExpenseAdapter
     private val expenses = arrayListOf<Expense>()
     private val expensesCollectionRef = FirebaseFirestore.getInstance().collection(EXPENSES_REF)
-    private lateinit var expenseListener : ListenerRegistration
+    private lateinit var expenseListener: ListenerRegistration
     private var runningTotal = 0.00
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +40,7 @@ class MainActivity : AppCompatActivity(), ExpenseDeleteItemClickListener {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { _ ->
-            val addExpenseIntent = Intent (this, AddExpenseActivity::class.java)
+            val addExpenseIntent = Intent(this, AddExpenseActivity::class.java)
             startActivity(addExpenseIntent)
         }
 
@@ -68,48 +69,48 @@ class MainActivity : AppCompatActivity(), ExpenseDeleteItemClickListener {
         builder.setView(dialogView)
                 .setTitle("Delete Item?")
                 .setMessage("Are you sure you want to delete this expense item? This cannot be undone.")
-                .setNegativeButton("Cancel"){_,_ ->}
-                .setPositiveButton("Yes"){ _, _ ->
+                .setNegativeButton("Cancel") { _, _ -> }
+                .setPositiveButton("Yes") { _, _ ->
                     expensesCollectionRef.document(expense.documentId).delete()
-                            .addOnSuccessListener {  }
+                            .addOnSuccessListener {
+                            }
                             .addOnFailureListener { exception ->
                                 Log.e("Error", "Could not delete expense: ${exception.localizedMessage}")
                             }
                     runningTotal -= expense.price
                     updateRunningTotal()
                 }
-        builder.show()
     }
 
-    private fun setListener () {
+    private fun setListener() {
         expenseListener = expensesCollectionRef
                 .orderBy(TIMESTAMP, Query.Direction.DESCENDING)
                 .whereEqualTo(USERNAME, auth.currentUser?.displayName.toString())
                 .addSnapshotListener(this) { snapshot, exception ->
-            if (exception != null) {
-                Log.e("Error", "Could not retrieve expenses: ${exception.localizedMessage}")
-            }
+                    if (exception != null) {
+                        Log.e("Error", "Could not retrieve expenses: ${exception.localizedMessage}")
+                    }
 
-            if (snapshot != null) {
-                expenses.clear()
-                runningTotal = 0.00
-                for (document in snapshot.documents) {
-                    val data = document.data
-                    val category = data!![CATEGORY] as String
-                    val itemName = data[ITEM_NAME] as String
-                    val price = data[PRICE] as Double
-                    val timestamp = data[TIMESTAMP] as Date
-                    val username = data[USERNAME] as String
-                    val documentId = document.id
+                    if (snapshot != null) {
+                        expenses.clear()
+                        runningTotal = 0.00
+                        for (document in snapshot.documents) {
+                            val data = document.data
+                            val category = data!![CATEGORY] as String
+                            val itemName = data[ITEM_NAME] as String
+                            val price = data[PRICE] as Double
+                            val timestamp = data[TIMESTAMP] as Date
+                            val username = data[USERNAME] as String
+                            val documentId = document.id
 
-                    val newExpense = Expense(category, itemName, price, timestamp, username, documentId)
-                    expenses.add(newExpense)
+                            val newExpense = Expense(category, itemName, price, timestamp, username, documentId)
+                            expenses.add(newExpense)
 
-                    runningTotal += price
+                            runningTotal += price
+                        }
+                        updateRunningTotal()
+                    }
                 }
-                updateRunningTotal()
-            }
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -137,17 +138,49 @@ class MainActivity : AppCompatActivity(), ExpenseDeleteItemClickListener {
                 auth.signOut()
                 expenses.clear()
                 runningTotal = 0.00
-                totalSpentLabel.text = format (Locale.getDefault(),"$%,.2f", runningTotal)
+                totalSpentLabel.text = format(Locale.getDefault(), "$%,.2f", runningTotal)
                 expenseAdapter.notifyDataSetChanged()
             }
             return true
+        } else {
+            //delete all expenses and update running total
+            //Open "Are  yo sure?" dialog before deleting
+            deleteCollection { success ->
+                if (success) {
+                    runningTotal = 0.00
+                    updateRunningTotal()
+                }
+            }
         }
         return false
     }
 
-    fun updateRunningTotal () {
+    private fun updateRunningTotal() {
         val totalSpent = findViewById<TextView>(R.id.totalSpentLabel)
-        totalSpent.text = format (Locale.getDefault(),"$%,.2f", runningTotal)
+        totalSpent.text = format(Locale.getDefault(), "$%,.2f", runningTotal)
         expenseAdapter.notifyDataSetChanged()
+    }
+
+    fun deleteCollection(complete: (Boolean) -> Unit) {
+        val query = expensesCollectionRef.whereEqualTo(USERNAME, auth.currentUser?.displayName.toString())
+        query.get().addOnSuccessListener { snapshot ->
+            thread {
+                val batch = FirebaseFirestore.getInstance().batch()
+                for (document in snapshot) {
+                    val docRef = FirebaseFirestore.getInstance().collection(EXPENSES_REF).document(document.id)
+                    batch.delete(docRef)
+                }
+                batch.commit()
+                        .addOnSuccessListener {
+                            complete(true)
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.e("Error", "Could not delete all expenses: ${exception.localizedMessage}")
+                        }
+            }
+        }
+                .addOnFailureListener {exception ->
+                    Log.e("Error", "Could not retrieve all expenses: ${exception.localizedMessage}")
+                }
     }
 }
