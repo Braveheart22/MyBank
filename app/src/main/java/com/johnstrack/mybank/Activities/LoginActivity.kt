e9d4d8fdbdd4dc5da1f04d7e79118ad88ab4f7f9
John Strack <Braveheart22@GMail.com>
2018-06-07 15:49:53 -0400

    Google sign in integration

--- app/src/main/java/com/johnstrack/mybank/Activities/LoginActivity.kt
+++ app/src/main/java/com/johnstrack/mybank/Activities/LoginActivity.kt
@@ -2,28 +2,19 @@ package com.johnstrack.mybank.Activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.johnstrack.mybank.R
 import kotlinx.android.synthetic.main.activity_login.*
-import android.support.design.widget.Snackbar
-import jdk.nashorn.internal.runtime.ECMAException.getException
-import com.google.firebase.auth.FirebaseUser
-import org.junit.experimental.results.ResultMatchers.isSuccessful
-import com.google.firebase.auth.AuthResult
-import com.google.android.gms.tasks.Task
-import android.support.annotation.NonNull
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.AuthCredential
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 
 
 
@@ -69,6 +60,13 @@ class LoginActivity : AppCompatActivity() {
                 }
     }
 
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -89,24 +87,23 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-        Log.d("Debug", "firebaseAuthWithGoogle:" + acct.id!!)
+        Log.d("Debug", "firebaseAuthWithGoogle: ${acct.id}")
 
         val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
         auth.signInWithCredential(credential)
-                .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+                .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(FragmentActivity.TAG, "signInWithCredential:success")
-                        val user = auth.getCurrentUser()
-                        updateUI(user)
+                        Log.d("Debug", "signInWithCredential:success")
+                        updateUI(auth.currentUser)
                     } else {
-                        // If sign in fails, display a message to the user.
                         Log.e("Error", "signInWithCredential:failure", task.exception)
-                        Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Authentication with Firestore Failed.", Toast.LENGTH_SHORT).show()
                         updateUI(null)
                     }
+                }
+    }
 
-                    // ...
-                })
+    private fun updateUI(user: FirebaseUser?) {
+        Log.d("Debug", "You don't really need this... ${user?.uid}")
     }
 }
